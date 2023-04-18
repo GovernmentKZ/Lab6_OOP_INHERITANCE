f5295948f13daa204b0f216ba28c92d4ef79a7fb
Date <114135922+GovernmentKZ@users.noreply.github.com>
2023-04-18 14:34:26 +0600

    Task1

---
+++ Shape.java
@@ -0,0 +1,12 @@
+public class Shape {
+    public String shapeName;
+
+    public Shape(String shapeName) {
+
+        this.shapeName = shapeName;
+    }
+
+    public String getShapeName() {
+        return shapeName;
+    }
+}
---
+++ Triangle.java
@@ -0,0 +1,5 @@
+public class Triangle extends Shape{
+    public Triangle(String shapeName){
+        super(shapeName);
+    }
+}
